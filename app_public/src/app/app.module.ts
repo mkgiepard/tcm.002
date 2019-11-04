ffb891e2195dcd6b1ac5dd678dadf0f138801647
mkgiepard <mkgiepard@gmail.com>
2019-11-04 22:30:35 +0100

    fe: adding router, top bar and side bar

--- app_public/src/app/app.module.ts
+++ app_public/src/app/app.module.ts
@@ -1,14 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import {
+  MatToolbarModule,
+  MatIconModule, 
+  MatSidenavModule, 
+  MatListModule,
+  MatButtonModule, 
+  MatFormFieldModule, 
+  MatInputModule, 
+  MatCardModule, 
+  MatTableModule 
+} from '@angular/material';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule, 
+    ReactiveFormsModule, 
+    MatInputModule,
+    CommonModule, 
+    MatButtonModule,
+    MatToolbarModule, 
+    MatIconModule, 
+    MatSidenavModule, 
+    MatListModule, 
+    MatFormFieldModule, 
+    MatCardModule, 
+    MatTableModule, 
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
