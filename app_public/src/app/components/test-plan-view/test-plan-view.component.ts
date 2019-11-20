ab1751e8073b26e771555f0f8363713d248e1470
mkgiepard <mkgiepard@gmail.com>
2019-11-20 21:54:27 +0100

    fe: adding test-plan-view

--- app_public/src/app/components/test-plan-view/test-plan-view.component.ts
+++ app_public/src/app/components/test-plan-view/test-plan-view.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { TestPlan, TestCase, TEST_PLAN_DATA} from '../../models/test-plan.model';
 
 @Component({
   selector: 'app-test-plan-view',
   templateUrl: './test-plan-view.component.html',
-  styleUrls: ['./test-plan-view.component.css']
+  styleUrls: ['./test-plan-view.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('25ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class TestPlanViewComponent implements OnInit {
+  displayedColumns: string[] = ['index', 'title', 'priority', 'status'];
+  columnsToDisplay = ['index', 'title', 'priority', 'status'];
+  testPlanData = TEST_PLAN_DATA;
+  dataSource = TEST_PLAN_DATA.testCases;
 
+  expandedElement: TestCase | null;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  isGroup(index, item): boolean {
+    return item.groupIndex ? true : false;
+  }
+
+  isTest(index, item): boolean {
+    return item.title ? true : false;
   }
 
 }
