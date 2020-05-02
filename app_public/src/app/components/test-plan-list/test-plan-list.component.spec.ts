a4203063b5d26bf69f51dd30a6ca9fe27488245f
Mariusz <mkgiepard@gmail.com>
2020-05-02 18:56:59 +0200

    fe: adding action icons to the testPlanList page

--- app_public/src/app/components/test-plan-list/test-plan-list.component.spec.ts
+++ app_public/src/app/components/test-plan-list/test-plan-list.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppMaterialModule } from '../../app.material.module';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { TestPlanListComponent } from './test-plan-list.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('TestPlanListComponent', () => {
   let component: TestPlanListComponent;
@@ -10,7 +11,7 @@ describe('TestPlanListComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ TestPlanListComponent ],
-      imports: [AppMaterialModule, NoopAnimationsModule]
+      imports: [AppMaterialModule, NoopAnimationsModule, RouterTestingModule]
     })
     .compileComponents();
   }));
