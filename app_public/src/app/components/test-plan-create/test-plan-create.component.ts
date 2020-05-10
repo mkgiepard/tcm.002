784c17cd167f77e9f8c2e625af645a31f496861a
Mariusz <mkgiepard@gmail.com>
2020-05-10 12:51:47 +0200

    fe: using formatter in test plan component

--- app_public/src/app/components/test-plan-create/test-plan-create.component.ts
+++ app_public/src/app/components/test-plan-create/test-plan-create.component.ts
@@ -1,29 +1,55 @@
-import { Component, OnInit } from '@angular/core';
-import { animate, state, style, transition, trigger } from '@angular/animations';
-import { TestPlan, TestCase, TestCaseGroup } from '../../models/test-plan.model';
+import { Component, OnInit } from "@angular/core";
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from "@angular/animations";
+import {
+  TestPlan,
+  TestCase,
+  TestCaseGroup,
+} from "../../models/test-plan.model";
 
 @Component({
-  selector: 'app-test-plan-create',
-  templateUrl: './test-plan-create.component.html',
-  styleUrls: ['./test-plan-create.component.css'],
+  selector: "app-test-plan-create",
+  templateUrl: "./test-plan-create.component.html",
+  styleUrls: ["./test-plan-create.component.css"],
   animations: [
-    trigger('detailExpand', [
-      state('collapsed', style({height: '0px', minHeight: '0'})),
-      state('expanded', style({height: '*'})),
-      transition('expanded <=> collapsed', animate('25ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    trigger("detailExpand", [
+      state("collapsed", style({ height: "0px", minHeight: "0" })),
+      state("expanded", style({ height: "*" })),
+      transition(
+        "expanded <=> collapsed",
+        animate("25ms cubic-bezier(0.4, 0.0, 0.2, 1)")
+      ),
     ]),
   ],
 })
-
 export class TestPlanCreateComponent implements OnInit {
-  columnsToDisplay = ['index', 'title', 'priority', 'status', 'action'];
-  
+  columnsToDisplay = ["index", "title", "priority", "status", "action"];
+
   testCaseData: (TestCase | TestCaseGroup)[] = [
-    {groupIndex: '1.1', groupName: 'Functional > CUJ', isGroupBy: true},
-    {index: 1, title: 'Verify if a User can sign in with a newly created account', priority: 'P0', status: 'NEW',
-     steps: 'Preconditions: xxxxxxxxxxxxxxxxxxxx;\n\nStep 1: aaaaaaaa;\nStep 2: bbbbbbbb;\nStep 3: ccccccc'},
-    {index: 2, title: 'Verify if already created User can sign in', priority: 'P1', status: 'NEW',
-    steps: 'Preconditions: xxxxxxxxxxxxxxxxxxxx; Step 1: aaaaaaaa; Step 2: bbbbbbbb; Step 3: ccccccc'},
+    { groupIndex: "1.1", groupName: "Functional > CUJ", isGroupBy: true },
+    {
+      index: 1,
+      title: "Verify if a User can sign in with a newly created account",
+      priority: "P0",
+      status: "NEW",
+      steps:
+        "Preconditions: xxxxxxxxxxxxxxxxxxxx;\n\nStep 1: aaaaaaaa;\nStep 2: bbbbbbbb;\n" +
+        "Step 3: ccccccc",
+    },
+    {
+      index: 2,
+      title: "Verify if already created User can sign in",
+      priority: "P1",
+      status: "NEW",
+      steps:
+        "Preconditions: xxxxxxxxxxxxxxxxxxxx; Step 1: aaaaaaaa; Step 2: bbbbbbbb; " +
+        "Step 3: ccccccc",
+    },
   ];
   testPlanData = {
     testCases: this.testCaseData,
@@ -31,10 +57,9 @@ export class TestPlanCreateComponent implements OnInit {
 
   expandedElement: TestCase | null;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   isGroup(index, item): boolean {
     return item.groupIndex ? true : false;
@@ -43,5 +68,4 @@ export class TestPlanCreateComponent implements OnInit {
   isTest(index, item): boolean {
     return item.title ? true : false;
   }
-
 }
--- app_public/src/app/models/test-plan.model.ts
+++ app_public/src/app/models/test-plan.model.ts
@@ -1,83 +1,231 @@
 export interface TestPlan {
-    id: string;
-    name: string;
-    author: string;
-    create_date: Date;
-    testCases: (TestCase | TestCaseGroup)[];
+  id: string;
+  name: string;
+  author: string;
+  create_date: Date;
+  testCases: (TestCase | TestCaseGroup)[];
 }
 
 export interface TestCase {
-    id?: string; // TODO: make it mandatory
-    title: string;
-    index: number;
-    priority: string;
-    status: string;
-    steps: string;
-  }
+  id?: string; // TODO: make it mandatory
+  title: string;
+  index: number;
+  priority: string;
+  status: string;
+  steps: string;
+}
 
 export interface TestCaseGroup {
-    id?: string; // TODO: make it mandatory
-    groupIndex: string;
-    groupName: string;
-    isGroupBy: boolean;
+  id?: string; // TODO: make it mandatory
+  groupIndex: string;
+  groupName: string;
+  isGroupBy: boolean;
 }
 
 export const TEST_CASES_DATA: (TestCase | TestCaseGroup)[] = [
-    {groupIndex: '1.1', groupName: 'Functional > CUJ', isGroupBy: true},
-    {index: 1, title: 'Verify if a User can sign in with a newly created account', priority: 'P0', status: 'NEW',
-     steps: 'Preconditions: xxxxxxxxxxxxxxxxxxxx;\n\nStep 1: aaaaaaaa;\nStep 2: bbbbbbbb;\nStep 3: ccccccc'},
-    {index: 2, title: 'Verify if already created User can sign in', priority: 'P1', status: 'NEW',
-    steps: 'Preconditions: xxxxxxxxxxxxxxxxxxxx; Step 1: aaaaaaaa; Step 2: bbbbbbbb; Step 3: ccccccc'},
-    {groupIndex: '1.2', groupName: 'Functional > non CUJ', isGroupBy: true},
-    {index: 1, title: 'Verify event recurring event creation with custom recurring rule', priority: 'P2', status: 'NEW',
-    steps: 'Preconditions: xxxxxxxxxxxxxxxxxxxx; Step 1: aaaaaaaa; Step 2: bbbbbbbb; Step 3: ccccccc'},
-    {groupIndex: '1.3', groupName: 'Functional > Error Cases', isGroupBy: true},
-    {index: 1, title: 'Verify event update with a start time before end time', priority: 'P3', status: 'NEW',
-    steps: 'Preconditions: xxxxxxxxxxxxxxxxxxxx; Step 1: aaaaaaaa; Step 2: bbbbbbbb; Step 3: ccccccc'},
-    {groupIndex: '1.4', groupName: 'Functional > Corner Cases', isGroupBy: true},
-    {index: 1, title: 'Verify recurring event exception update', priority: 'P4', status: 'NEW',
-    steps: 'Preconditions: xxxxxxxxxxxxxxxxxxxx; Step 1: aaaaaaaa; Step 2: bbbbbbbb; Step 3: ccccccc'},
-    {groupIndex: '2', groupName: 'Layout', isGroupBy: true},
-    {index: 1, title: 'Verify create bubble with long strings', priority: 'P5', status: 'NEW',
-    steps: 'Preconditions: xxxxxxxxxxxxxxxxxxxx; Step 1: aaaaaaaa; Step 2: bbbbbbbb; Step 3: ccccccc'},
-    {index: 2, title: 'Verify month view on HD resolution screen', priority: 'P6', status: 'NEW',
-    steps: 'Preconditions: xxxxxxxxxxxxxxxxxxxx; Step 1: aaaaaaaa; Step 2: bbbbbbbb; Step 3: ccccccc'},
-    {groupIndex: '3', groupName: 'Navigation', isGroupBy: true},
-    {index: 1, title: 'Verify Back button funtionlity on create event bubble', priority: 'P7', status: 'NEW',
-    steps: 'Preconditions: xxxxxxxxxxxxxxxxxxxx; Step 1: aaaaaaaa; Step 2: bbbbbbbb; Step 3: ccccccc'},
-    {groupIndex: '4', groupName: 'A11y', isGroupBy: true},
-    {index: 1, title: 'Check CUJ in a11y mode', priority: 'P8', status: 'NEW',
-    steps: 'Preconditions: xxxxxxxxxxxxxxxxxxxx; Step 1: aaaaaaaa; Step 2: bbbbbbbb; Step 3: ccccccc'},
-    {index: 2, title: 'Check UI with a11y color inspector', priority: 'P9', status: 'NEW',
-    steps: 'Preconditions: xxxxxxxxxxxxxxxxxxxx; Step 1: aaaaaaaa; Step 2: bbbbbbbb; Step 3: ccccccc'},
-    {groupIndex: '5', groupName: 'Performance', isGroupBy: true},
-    {index: 1, title: 'Check the app loading time with enormous set of data', priority: 'P10', status: 'NEW',
-    steps: 'Preconditions: xxxxxxxxxxxxxxxxxxxx; Step 1: aaaaaaaa; Step 2: bbbbbbbb; Step 3: ccccccc'},
-  ];
+  { groupIndex: "1.1", groupName: "Functional > CUJ", isGroupBy: true },
+  {
+    index: 1,
+    title: "Verify if a User can sign in with a newly created account",
+    priority: "P0",
+    status: "NEW",
+    steps:
+      "Preconditions: xxxxxxxxxxxxxxxxxxxx;\n\nStep 1: aaaaaaaa;\nStep 2: bbbbbbbb;\nStep 3: ccccccc",
+  },
+  {
+    index: 2,
+    title: "Verify if already created User can sign in",
+    priority: "P1",
+    status: "NEW",
+    steps:
+      "Preconditions: xxxxxxxxxxxxxxxxxxxx; Step 1: aaaaaaaa; Step 2: bbbbbbbb; Step 3: ccccccc",
+  },
+  { groupIndex: "1.2", groupName: "Functional > non CUJ", isGroupBy: true },
+  {
+    index: 1,
+    title: "Verify event recurring event creation with custom recurring rule",
+    priority: "P2",
+    status: "NEW",
+    steps:
+      "Preconditions: xxxxxxxxxxxxxxxxxxxx; Step 1: aaaaaaaa; Step 2: bbbbbbbb; Step 3: ccccccc",
+  },
+  { groupIndex: "1.3", groupName: "Functional > Error Cases", isGroupBy: true },
+  {
+    index: 1,
+    title: "Verify event update with a start time before end time",
+    priority: "P3",
+    status: "NEW",
+    steps:
+      "Preconditions: xxxxxxxxxxxxxxxxxxxx; Step 1: aaaaaaaa; Step 2: bbbbbbbb; Step 3: ccccccc",
+  },
+  {
+    groupIndex: "1.4",
+    groupName: "Functional > Corner Cases",
+    isGroupBy: true,
+  },
+  {
+    index: 1,
+    title: "Verify recurring event exception update",
+    priority: "P4",
+    status: "NEW",
+    steps:
+      "Preconditions: xxxxxxxxxxxxxxxxxxxx; Step 1: aaaaaaaa; Step 2: bbbbbbbb; Step 3: ccccccc",
+  },
+  { groupIndex: "2", groupName: "Layout", isGrśoupBy: true },
+  {
+    index: 1,
+    title: "Verify create bubble with long strings",
+    priority: "P5",
+    status: "NEW",
+    steps:
+      "Preconditions: xxxxxxxxxxxxxxxxxxxx; Step 1: aaaaaaaa; Step 2: bbbbbbbb; Step 3: ccccccc",
+  },
+  {
+    index: 2,
+    title: "Verify month view on HD resolution screen",
+    priority: "P6",
+    status: "NEW",
+    steps:
+      "Preconditions: xxxxxxxxxxxxxxxxxxxx; Step 1: aaaaaaaa; Step 2: bbbbbbbb; Step 3: ccccccc",
+  },
+  { groupIndex: "3", groupName: "Navigation", isGroupBy: true },
+  {
+    index: 1,
+    title: "Verify Back button funtionlity on create event bubble",
+    priority: "P7",
+    status: "NEW",
+    steps:
+      "Preconditions: xxxxxxxxxxxxxxxxxxxx; Step 1: aaaaaaaa; Step 2: bbbbbbbb; Step 3: ccccccc",
+  },
+  { groupIndex: "4", groupName: "A11y", isGroupBy: true },
+  {
+    index: 1,
+    title: "Check CUJ in a11y mode",
+    priority: "P8",
+    status: "NEW",
+    steps:
+      "Preconditions: xxxxxxxxxxxxxxxxxxxx; Step 1: aaaaaaaa; Step 2: bbbbbbbb; Step 3: ccccccc",
+  },
+  {
+    index: 2,
+    title: "Check UI with a11y color inspector",
+    priority: "P9",
+    status: "NEW",
+    steps:
+      "Preconditions: xxxxxxxxxxxxxxxxxxxx; Step 1: aaaaaaaa; Step 2: bbbbbbbb; Step 3: ccccccc",
+  },
+  { groupIndex: "5", groupName: "Performance", isGroupBy: true },
+  {
+    index: 1,
+    title: "Check the app loading time with enormous set of data",
+    priority: "P10",
+    status: "NEW",
+    steps:
+      "Preconditions: xxxxxxxxxxxxxxxxxxxx; Step 1: aaaaaaaa; Step 2: bbbbbbbb; Step 3: ccccccc",
+  },
+];
 
 export const TEST_PLAN_DATA: TestPlan = {
-    id: '1',
-    name: 'Test Plan A.001',
-    author: 'Mario Bros',
-    create_date: new Date('2019-11-17'),
-    testCases: TEST_CASES_DATA,
+  id: "1",
+  name: "Test Plan A.001",
+  author: "Mario Bros",
+  create_date: new Date("2019-11-17"),
+  testCases: TEST_CASES_DATA,
 };
 
 export const TEST_PLAN_DATA_MULTI: TestPlan[] = [
-  {id: '1', name: 'Test Plan A.001', author: 'Mario Bros', create_date: new Date('2019-11-17'), testCases: TEST_CASES_DATA},
-  {id: '2', name: 'Test Plan B.002', author: 'Mario Bros', create_date: new Date('2019-11-17'), testCases: TEST_CASES_DATA},
-  {id: '3', name: 'Test Plan C.003', author: 'Mario Bros', create_date: new Date('2019-11-17'), testCases: TEST_CASES_DATA},
-  {id: '4', name: 'Test Plan D.001', author: 'Mario Bros', create_date: new Date('2019-11-17'), testCases: TEST_CASES_DATA},
-  {id: '5', name: 'Test Plan E.002', author: 'Mario Bros', create_date: new Date('2019-11-17'), testCases: TEST_CASES_DATA},
-  {id: '6', name: 'Test Plan F.003', author: 'Mario Bros', create_date: new Date('2019-11-17'), testCases: TEST_CASES_DATA},
-  {id: '7', name: 'Test Plan G.001', author: 'Mario Bros', create_date: new Date('2019-11-17'), testCases: TEST_CASES_DATA},
-  {id: '8', name: 'Test Plan H.002', author: 'Mario Bros', create_date: new Date('2019-11-17'), testCases: TEST_CASES_DATA},
-  {id: '9', name: 'Test Plan I.003', author: 'Mario Bros', create_date: new Date('2019-11-17'), testCases: TEST_CASES_DATA},
-  {id: '10', name: 'Test Plan J.001', author: 'Mario Bros', create_date: new Date('2019-11-17'), testCases: TEST_CASES_DATA},
-  {id: '11', name: 'Test Plan K.001', author: 'Mario Bros', create_date: new Date('2019-11-17'), testCases: TEST_CASES_DATA},
+  {
+    id: "1",
+    name: "Test Plan A.001",
+    author: "Mario Bros",
+    create_date: new Date("2019-11-17"),
+    testCases: TEST_CASES_DATA,
+  },
+  {
+    id: "2",
+    name: "Test Plan B.002",
+    author: "Mario Bros",
+    create_date: new Date("2019-11-17"),
+    testCases: TEST_CASES_DATA,
+  },
+  {
+    id: "3",
+    name: "Test Plan C.003",
+    author: "Mario Bros",
+    create_date: new Date("2019-11-17"),
+    testCases: TEST_CASES_DATA,
+  },
+  {
+    id: "4",
+    name: "Test Plan D.001",
+    author: "Mario Bros",
+    create_date: new Date("2019-11-17"),
+    testCases: TEST_CASES_DATA,
+  },
+  {
+    id: "5",
+    name: "Test Plan E.002",
+    author: "Mario Bros",
+    create_date: new Date("2019-11-17"),
+    testCases: TEST_CASES_DATA,
+  },
+  {
+    id: "6",
+    name: "Test Plan F.003",
+    author: "Mario Bros",
+    create_date: new Date("2019-11-17"),
+    testCases: TEST_CASES_DATA,
+  },
+  {
+    id: "7",
+    name: "Test Plan G.001",
+    author: "Mario Bros",
+    create_date: new Date("2019-11-17"),
+    testCases: TEST_CASES_DATA,
+  },
+  {
+    id: "8",
+    name: "Test Plan H.002",
+    author: "Mario Bros",
+    create_date: new Date("2019-11-17"),
+    testCases: TEST_CASES_DATA,
+  },
+  {
+    id: "9",
+    name: "Test Plan I.003",
+    author: "Mario Bros",
+    create_date: new Date("2019-11-17"),
+    testCases: TEST_CASES_DATA,
+  },
+  {
+    id: "10",
+    name: "Test Plan J.001",
+    author: "Mario Bros",
+    create_date: new Date("2019-11-17"),
+    testCases: TEST_CASES_DATA,
+  },
+  {
+    id: "11",
+    name: "Test Plan K.001",
+    author: "Mario Bros",
+    create_date: new Date("2019-11-17"),
+    testCases: TEST_CASES_DATA,
+  },
 ];
 
 export const TEST_PLAN_DATA_SELECTED: TestPlan[] = [
-{id: '1', name: 'Test Plan L.001', author: 'Mario Bros', create_date: new Date('2019-11-17'), testCases: TEST_CASES_DATA},
-{id: '2', name: 'Test Plan M.002', author: 'Mario Bros', create_date: new Date('2019-11-17'), testCases: TEST_CASES_DATA},
-]
+  {
+    id: "1",
+    name: "Test Plan L.001",
+    author: "Mario Bros",
+    create_date: new Date("2019-11-17"),
+    testCases: TEST_CASES_DATA,
+  },
+  {
+    id: "2",
+    name: "Test Plan M.002",
+    author: "Mario Bros",
+    create_date: new Date("2019-11-17"),
+    testCases: TEST_CASES_DATA,
+  },
+];
