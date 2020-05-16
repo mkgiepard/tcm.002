3e5abeef70e2324450c66653175dd8fda2fd4118
Mariusz <mkgiepard@gmail.com>
2020-05-16 12:07:02 +0200

    fe: fixing project-edit to show the data  after switching to in-memory-web-api

--- app_public/src/app/components/project-edit/project-edit.component.spec.ts
+++ app_public/src/app/components/project-edit/project-edit.component.spec.ts
@@ -1,14 +1,21 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { AppMaterialModule } from '../../app.material.module';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { ProjectEditComponent } from './project-edit.component';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { RouterTestingModule } from '@angular/router/testing';
-import { DebugElement } from '@angular/core';
-import { ProjectService } from '../../services/project.service';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { AppMaterialModule } from "../../app.material.module";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { ProjectEditComponent } from "./project-edit.component";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { RouterTestingModule } from "@angular/router/testing";
+import { DebugElement } from "@angular/core";
+import { ProjectService } from "../../services/project.service";
+// Http testing module and mocking controller
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from "@angular/common/http/testing";
+import { Observable, of } from "rxjs";
+import { Project } from "../../models/project.model";
 
-describe('ProjectEditComponent', () => {
+describe("ProjectEditComponent", () => {
   let component: ProjectEditComponent;
   let fixture: ComponentFixture<ProjectEditComponent>;
   let rootElement: DebugElement;
@@ -16,31 +23,35 @@ describe('ProjectEditComponent', () => {
   const projectServiceStub = {
     project: {
       id: 1,
-      name: 'testing',
-      subtitle: 'subtitle-testing',
-      author: 'joe deo',
-      status: 'NEW',
-      create_date: new Date('2019-11-12')
+      name: "testing",
+      subtitle: "subtitle-testing",
+      author: "joe deo",
+      status: "NEW",
+      create_date: new Date("2019-11-12"),
     },
 
-    getProjectById: async function(id: number) {
+    getProjectById(id: number): Observable<Project> {
       component.project = this.project;
-      return this.project;
+      return of(this.project);
     },
 
-    updateProject: async function(project) {
+    updateProject: async function (project) {
       component.project = project;
     },
   };
 
-
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProjectEditComponent ],
-      imports: [AppMaterialModule, NoopAnimationsModule, ReactiveFormsModule, RouterTestingModule],
-      providers: [{provide: ProjectService, useValue: projectServiceStub}]
-    })
-    .compileComponents();
+      declarations: [ProjectEditComponent],
+      imports: [
+        AppMaterialModule,
+        NoopAnimationsModule,
+        ReactiveFormsModule,
+        RouterTestingModule,
+        HttpClientTestingModule,
+      ],
+      providers: [{ provide: ProjectService, useValue: projectServiceStub }],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -49,7 +60,7 @@ describe('ProjectEditComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
--- app_public/src/app/components/project-edit/project-edit.component.ts
+++ app_public/src/app/components/project-edit/project-edit.component.ts
@@ -1,17 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { Project } from '../../models/project.model';
-import { ProjectService } from '../../services/project.service';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { Project } from "../../models/project.model";
+import { ProjectService } from "../../services/project.service";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-project-edit',
-  templateUrl: './project-edit.component.html',
-  styleUrls: ['./project-edit.component.css']
+  selector: "app-project-edit",
+  templateUrl: "./project-edit.component.html",
+  styleUrls: ["./project-edit.component.css"],
 })
 export class ProjectEditComponent implements OnInit {
   id: number;
-  project: any = {};
+  project: Project;
   updateForm: FormGroup;
 
   constructor(
@@ -21,28 +21,34 @@ export class ProjectEditComponent implements OnInit {
     private fb: FormBuilder
   ) {
     this.createForm();
-   }
+  }
 
-  ngOnInit() {
-    this.route.params.subscribe( params => {
-      this.id = params.id;
-      this.project = this.projectService.getProjectById(Number(params.id));
-      this.updateForm.get('name').setValue(this.project.name);
-      this.updateForm.get('subtitle').setValue(this.project.subtitle);
-      this.updateForm.get('author').setValue(this.project.author);
-      this.updateForm.get('status').setValue(this.project.status);
+  ngOnInit(): void {
+    this.getProject();
+  }
+
+  getProject(): void {
+    const id = +this.route.snapshot.paramMap.get("id");
+    this.projectService.getProjectById(id).subscribe((project) => {
+      this.project = project;
+      this.updateForm.get("name").setValue(this.project.name);
+      this.updateForm.get("subtitle").setValue(this.project.subtitle);
+      this.updateForm.get("author").setValue(this.project.author);
+      this.updateForm.get("status").setValue(this.project.status);
       if (this.project.create_date != null) {
-        this.updateForm.get('create_date').setValue(this.project.create_date.toLocaleString());
+        this.updateForm
+          .get("create_date")
+          .setValue(this.project.create_date.toLocaleString());
       }
     });
   }
 
   createForm() {
-    this.updateForm = this.fb.group( {
-      name: ['', Validators.required],
-      subtitle: '',
-      author: '',
-      status: '',
+    this.updateForm = this.fb.group({
+      name: ["", Validators.required],
+      subtitle: "",
+      author: "",
+      status: "",
       create_date: null,
     });
   }
@@ -50,7 +56,6 @@ export class ProjectEditComponent implements OnInit {
   updateProject(project) {
     project.id = Number(this.id);
     this.projectService.updateProject(project);
-    this.router.navigate(['/project-list']);
+    this.router.navigate(["/project-list"]);
   }
-
 }
