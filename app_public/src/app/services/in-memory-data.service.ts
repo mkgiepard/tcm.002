7fffebeca71e571f579de20526bd638e01dcf102
Mariusz <mkgiepard@gmail.com>
2020-06-30 21:20:39 +0200

    fe: move test-case-group and test-case data to in-memory-database

--- app_public/src/app/services/in-memory-data.service.ts
+++ app_public/src/app/services/in-memory-data.service.ts
@@ -8,6 +8,8 @@ import {
   TEST_PLAN_DATA_MULTI,
 } from "../models/test-plan.model";
 import { PROJECT_DATA } from "../models/fake-data/project-data";
+import { TEST_CASE_GROUP_DATA } from "../models/fake-data/test-case-group-data";
+import { TEST_CASE_DATA } from "../models/fake-data/test-case-data";
 
 @Injectable({
   providedIn: "root",
@@ -15,198 +17,14 @@ import { PROJECT_DATA } from "../models/fake-data/project-data";
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const projects: Project[] = PROJECT_DATA;
-    const testGroups: TestCaseGroup[] = [
-      {
-        id: 1,
-        index: "1",
-        name: "Functional",
-        author_id: 1,
-        parent_id: 0,
-        project_id: 1,
-        test_plan_id: 1,
-        isGroupBy: true,
-        created: new Date("2020-05-23"),
-      },
-      {
-        id: 2,
-        index: "1.1",
-        name: "Functional > UI",
-        author_id: 1,
-        parent_id: 1,
-        project_id: 1,
-        test_plan_id: 1,
-        isGroupBy: true,
-        created: new Date("2020-05-23"),
-      },
-      {
-        id: 3,
-        index: "1.2",
-        name: "Functional > CUJ",
-        author_id: 1,
-        parent_id: 1,
-        project_id: 1,
-        test_plan_id: 1,
-        isGroupBy: true,
-        created: new Date("2020-05-23"),
-      },
-      {
-        id: 4,
-        index: "2",
-        name: "Performance",
-        author_id: 1,
-        parent_id: 0,
-        project_id: 1,
-        test_plan_id: 1,
-        isGroupBy: true,
-        created: new Date("2020-05-23"),
-      },
-      {
-        id: 5,
-        index: "2.1",
-        name: "Performance > Network",
-        author_id: 1,
-        parent_id: 4,
-        project_id: 1,
-        test_plan_id: 1,
-        isGroupBy: true,
-        created: new Date("2020-05-23"),
-      },
-      {
-        id: 6,
-        index: "3",
-        name: "Security",
-        author_id: 1,
-        parent_id: 0,
-        project_id: 1,
-        test_plan_id: 1,
-        isGroupBy: true,
-        created: new Date("2020-05-23"),
-      },
-    ];
-    const testCases: TestCase[] = [
-      {
-        id: 1,
-        index: 1,
-        title: "Verify if a User can sign in with a newly created account",
-        priority: "P2",
-        status: "NEW",
-        steps:
-          "1. Do something\n2. Then do something else\nVRF: AAA is not green",
-        tags: [],
-        author_id: 1,
-        group_id: 1,
-        project_id: 1,
-        test_plan_id: 1,
-        created: new Date("2020-05-23"),
-      },
-      {
-        id: 2,
-        index: 2,
-        title: "Verify if already created User can sign in",
-        priority: "P2",
-        status: "NEW",
-        steps: "",
-        tags: [],
-        author_id: 1,
-        group_id: 1,
-        project_id: 1,
-        test_plan_id: 1,
-        created: new Date("2020-05-23"),
-      },
-      {
-        id: 3,
-        index: 3,
-        title:
-          "Verify event recurring event creation with custom recurring rule",
-        priority: "P2",
-        status: "NEW",
-        steps: "",
-        tags: [],
-        author_id: 1,
-        group_id: 1,
-        project_id: 1,
-        test_plan_id: 1,
-        created: new Date("2020-05-23"),
-      },
-      {
-        id: 4,
-        index: 1,
-        title: "Test case A",
-        priority: "P2",
-        status: "NEW",
-        steps: "",
-        tags: [],
-        author_id: 1,
-        group_id: 2,
-        project_id: 1,
-        test_plan_id: 1,
-        created: new Date("2020-05-23"),
-      },
-      {
-        id: 5,
-        index: 2,
-        title: "Test case B",
-        priority: "P2",
-        status: "NEW",
-        steps: "",
-        tags: [],
-        author_id: 1,
-        group_id: 2,
-        project_id: 1,
-        test_plan_id: 1,
-        created: new Date("2020-05-23"),
-      },
-      {
-        id: 6,
-        index: 1,
-        title: "Test case C",
-        priority: "P2",
-        status: "NEW",
-        steps: "",
-        tags: [],
-        author_id: 1,
-        group_id: 3,
-        project_id: 1,
-        test_plan_id: 1,
-        created: new Date("2020-05-23"),
-      },
-      {
-        id: 7,
-        index: 1,
-        title: "Test case D",
-        priority: "P2",
-        status: "NEW",
-        steps: "",
-        tags: [],
-        author_id: 1,
-        group_id: 4,
-        project_id: 1,
-        test_plan_id: 1,
-        created: new Date("2020-05-23"),
-      },
-      {
-        id: 8,
-        index: 2,
-        title: "Test case E",
-        priority: "P2",
-        status: "NEW",
-        steps: "",
-        tags: [],
-        author_id: 1,
-        group_id: 4,
-        project_id: 1,
-        test_plan_id: 1,
-        created: new Date("2020-05-23"),
-      },
-    ];
+    const testGroups: TestCaseGroup[] = TEST_CASE_GROUP_DATA;
+    const testCases: TestCase[] = TEST_CASE_DATA;
     const testPlans: TestPlan[] = TEST_PLAN_DATA_MULTI;
     console.log("in-memory-data > " + projects.length);
     return { projects, testGroups, testCases, testPlans };
   }
 
-  genId(projects: Project[]): number {
-    return projects.length > 0
-      ? Math.max(...projects.map((project) => project.id)) + 1
-      : 11;
+  genId<T extends Project | TestCaseGroup | TestCase>(table: T[]): number {
+    return table.length > 0 ? Math.max(...table.map((t) => t.id)) + 1 : 11;
   }
 }
