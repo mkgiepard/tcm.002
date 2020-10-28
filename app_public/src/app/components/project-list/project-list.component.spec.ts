d19c14cca1eec2e4e39f0b1a7902c8772ca2c481
Mariusz <mkgiepard@gmail.com>
2020-10-28 20:51:48 +0100

    fe: using project-service stub in project-list component tests

--- app_public/src/app/components/project-list/project-list.component.spec.ts
+++ app_public/src/app/components/project-list/project-list.component.spec.ts
@@ -1,18 +1,53 @@
-import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import {
+  async,
+  fakeAsync,
+  ComponentFixture,
+  TestBed,
+  tick,
+} from "@angular/core/testing";
 import { AppMaterialModule } from "../../app.material.module";
 import { NoopAnimationsModule } from "@angular/platform-browser/animations";
 import { ProjectListComponent } from "./project-list.component";
 import { Project } from "../../models/project.model";
 import { RouterTestingModule } from "@angular/router/testing";
+import { Observable, of } from "rxjs";
 // Http testing module and mocking controller
 import {
   HttpClientTestingModule,
   HttpTestingController,
 } from "@angular/common/http/testing";
+import { ProjectService } from "src/app/services/project.service";
 
 describe("ProjectListComponent", () => {
   let component: ProjectListComponent;
   let fixture: ComponentFixture<ProjectListComponent>;
+
+  const projectServiceStub = {
+    projects: [
+      {
+        id: 1,
+        name: "Project A",
+        status: "New",
+        author: "Mario Bros",
+        create_date: new Date("2019.11.12"),
+      },
+    ],
+    getProjects: function () {
+      return of(this.projects);
+    },
+    getProjectById: function (id: number) {
+      return of(this.projects[0]);
+    },
+    addProject: function (project: Project) {
+      this.projects.push(project);
+    },
+    updateProject: function (project: Project) {
+      this.projects.splice(0, 1, project);
+    },
+    deleteProject: function (project: Project | number) {
+      this.projects = [];
+    },
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -23,6 +58,7 @@ describe("ProjectListComponent", () => {
         RouterTestingModule,
         HttpClientTestingModule,
       ],
+      providers: [{ provide: ProjectService, useValue: projectServiceStub }],
     }).compileComponents();
   }));
 
@@ -32,30 +68,17 @@ describe("ProjectListComponent", () => {
     fixture.detectChanges();
   });
 
-  it("should create", () => {
+  it("should create", fakeAsync(() => {
     expect(component).toBeTruthy();
-  });
-
-  it("should set instance correctly", () => {
-    expect(component).not.toBeNull();
-  });
+  }));
 
   it("should be no projects if there is no data", () => {
-    // TODO: this should use some kind of mock of a ProjectService
-    component.projects = [];
-    expect(component.projects.length).toBe(0);
+    projectServiceStub.projects = [];
+    component.fetchProjects();
+    expect(component.projects.length).toEqual(0);
   });
 
   it("should be projects if there are data", () => {
-    const newProject: Project = {
-      id: 999,
-      name: "Project A",
-      subtitle: "Sanity tests",
-      status: "New",
-      author: "Mario Bros",
-      create_date: new Date("2019.11.12"),
-    };
-    component.projects = [newProject];
-    expect(component.projects.length).toBe(1);
+    expect(component.projects.length).toEqual(1);
   });
 });
